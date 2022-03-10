06c35e9ec4c0d5d5ebebf610e68cb3b2b0ed0528
mohtashimKdfd <mohtashim.kamran@unthinkable.co>
2022-03-10 10:44:35 +0530

    rest falcon

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import json
 import falcon
-
+from addfile import adder
 
 class Home:
     def on_get(self, req, resp):
         data = req.media
-        # print(data['data'])
-        # content ={
-        #     "name" : "mohtashim",
-        #     "app" : "falcon"
-        # }
         content ={
             "name" : data['data'],
             "app" : "falcon"
         }
 
         resp.body = json.dumps(content)
+        resp.status = falcon.HTTP_OK
+        return resp
+
+    def on_post(self, req, resp):
+        request = req.media
+        content = {
+            "sum is" :  adder(request['a'], request['b'])
+        }
+
+        resp.body = json.dumps(content)
+        resp.status = falcon.HTTP_OK
         return resp
 
 api = falcon.App()
